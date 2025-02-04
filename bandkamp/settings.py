dadaadf3d4e16f58709cac39a561fb7e7c6788fe
Breno Garcia <BrenoGHeat@gmail.com>
2025-02-04 15:54:43 -0300

    update settings.py

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -127,7 +127,7 @@ if DATABASE_URL:
     DEBUG = False
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     STATICFILES_STORAGE = ("whitenoise.storage.CompressedManifestStaticFilesStorage")
 
 
